c0cf60bfad775d6839220fc2167ab4d339764bd0
ikoziy <ikoziy@forum.weall.lo>
2016-04-11 16:02:47 +0300

    первая загрузка

---
+++ app/src/main/java/ru/smekalin/calcpro/NormalActivity.java
@@ -0,0 +1,15 @@
+package ru.smekalin.calcpro;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by USER on 11.04.2016.
+ */
+public class NormalActivity  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_normal);
+    }
+}
