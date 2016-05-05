3d7b4f1de8ff5a4d4a4b01ac276de1b6bbc405e3
ikoziy <ikoziy@forum.weall.lo>
2016-05-05 16:40:12 +0300

    переделал активити - с маштабированием

---
+++ app/src/main/java/ru/smekalin/calcpro/Classes/SymbolInput.java
@@ -0,0 +1,12 @@
+package ru.smekalin.calcpro.Classes;
+
+/**
+ * Created by USER on 05.05.2016.
+ */
+public class SymbolInput {
+
+    public void inpSymbol(int id)
+    {
+
+    }
+}
--- app/src/main/java/ru/smekalin/calcpro/NormalActivity.java
+++ app/src/main/java/ru/smekalin/calcpro/NormalActivity.java
@@ -1,15 +1,38 @@
 package ru.smekalin.calcpro;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by USER on 11.04.2016.
  */
-public class NormalActivity  extends AppCompatActivity {
+public class NormalActivity  extends Activity {
+    Button b_1, b_2, b_3, b_4, b_5, b_6, b_7, b_8, b_9, b_0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal);
+
+        b_0 = (Button) findViewById(R.id.b0);
+        b_1 = (Button) findViewById(R.id.b1);
+        b_2 = (Button) findViewById(R.id.b2);
+        b_3 = (Button) findViewById(R.id.b3);
+        b_4 = (Button) findViewById(R.id.b4);
+        b_5 = (Button) findViewById(R.id.b5);
+        b_6 = (Button) findViewById(R.id.b6);
+        b_7 = (Button) findViewById(R.id.b7);
+        b_8 = (Button) findViewById(R.id.b8);
+        b_9 = (Button) findViewById(R.id.b9);
+
+        b_0.setOnClickListener(onClick(this));
     }
+
+    private View.OnClickListener onClick(NormalActivity normalActivity) {
+
+        return null;
+    }
+
 }
